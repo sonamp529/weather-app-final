64290643181b105690e7a3d2343a1eaf960c598f
sonamp529 <77584460+sonamp529@users.noreply.github.com>
2021-03-25 13:54:26 -0400

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,19 +1,38 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours(); 
+    if (hours<10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes<10) {
+        minutes = `0${minutes}`;
+    } 
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {   
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description; 
     humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed); 
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt*1000);
 }
 
 let apiKey = "9fd12ff10340c7c6ab802ba58ee60657";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Etobicoke&appid=${apiKey}&units=metric`;
+let city = "Etobicoke";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
